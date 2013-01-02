a4e0f28f9900b3d276dd535d67970a2fb7af7fab
Jon <jon@onwebcentric.com>
2013-01-02 13:33:35 -0800

    Header/Styling Fix
    Minor changes that were broken in the upgrade.
    Header.php
    *  Header is now an image
    *  No longer shows blog description
    Style.css
    *  .sidebar-sponsor images now properly show in a block
    *  Site title now has a background
    *  Navigation properly centered in wide display
    *  Remove wasted space at top

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -35,8 +35,8 @@
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="wp-content/uploads/2012/11/index.png" width="240" height="213" style="display: block;" /></a></h1>
+			<h2 class="site-description"><?php // bloginfo( 'description' ); Silence my friend ?></h2>
 		</hgroup>
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
