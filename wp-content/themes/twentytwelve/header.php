6fa5446e1483add9b001ee1ce40364d2ab88b8fc
Jon <jon@onwebcentric.com>
2013-01-02 13:46:14 -0800

    Site Title Fix
    * Home page only shows site title
    * Deeper pages follow this design:  "$PAGE_NAME | $SITE_NAME"

--- wp-content/themes/twentytwelve/header.php
+++ wp-content/themes/twentytwelve/header.php
@@ -21,7 +21,11 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
-<title><?php wp_title( '|', true, 'right' ); ?></title>
+<title>
+	<?php if( !is_front_page() ): wp_title( '|', true, 'right' ); ?>
+	<?php else: bloginfo('name'); ?>
+	<?php endif; ?>
+</title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php // Loads HTML5 JavaScript file to add support for HTML5 elements in older IE versions. ?>
